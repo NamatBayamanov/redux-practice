87e0bb5f2d0bafd068fc00933b00549ce774bb20
Namat Bayamanov <namataxvd15@gmail.com>
2022-06-21 09:16:01 +0600

    added router, sass, axios pekages

--- src/App.js
+++ src/App.js
@@ -1,9 +1,18 @@
+import {Routes, Route} from "react-router-dom"
+
 
 function App() {
   return (
     <div className="App">
+      {/* <Routes >
+        <Route path="" element=""/>
+
+      </Routes> */}
+
+
       
-    </div>
+    
+    </div>  
   );
 }
 
